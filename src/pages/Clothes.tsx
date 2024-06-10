2d71dceb1e8169a5ff4d41b4d690a7f74c3a90ba
HarlamovEvgeniy <e.harlamov@octogroup.io>
2024-06-10 12:14:01 +0300

    Update Clothes.tsx

--- src/pages/Clothes.tsx
+++ src/pages/Clothes.tsx
@@ -1,4 +1,13 @@
+import { TypographyH2 } from "@/components/ui/typography"
+import { ClothesForm, ClothesTable } from "@/feature/clothes"
+
 export const Clothes = () => {
 
-	return (<></>)
+	return (
+		<>
+			<TypographyH2>Новая накладная «Одежда»</TypographyH2>
+			<ClothesForm />
+			<ClothesTable />
+		</>
+	)
 }
